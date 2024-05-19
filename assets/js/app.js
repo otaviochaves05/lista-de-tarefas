358f8799606118659ed2776ecbe97d4bfc4903f7
Otavio Chaves <otaviocs2005@gmail.com>
2024-05-19 01:11:28 -0300

    Finalizado

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,21 +2,67 @@ const tarefaInput = document.querySelector("#tarefaInput");
 const dataInput = document.querySelector("#dataInput");
 const buttonSubmit = document.querySelector("#addTarefa");
 
-buttonSubmit.addEventListener("click",addTarefa);
+buttonSubmit.addEventListener("click", addTarefa);
 
 function addTarefa() {
     let tarefa = new Object();
     tarefa.id = obterID();
     tarefa.nome = tarefaInput.value;
     tarefa.data = dataInput.value;
-    tarefa.status = "Pendente"
-
+    tarefa.status = "Concluir"
 
     //populando objeto json
     let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
     tarefas.push(tarefa);
     localStorage.setItem("tarefas", JSON.stringify(tarefas));
 
+
+
+    //criando linha
+    const linha = document.createElement("tr");
+
+    //criando elementos da linha
+    const celulaNomeTarefa = document.createElement("td");
+    celulaNomeTarefa.innerText = tarefa.nome;
+
+    const celulaDataTarefa = document.createElement("td");
+    celulaDataTarefa.innerText = tarefa.data;
+
+    //botao status
+    const botaoStatus = document.createElement("button");
+    botaoStatus.innerText = tarefa.status;
+
+    //botao remover
+    const botaoRemover = document.createElement("button");
+    botaoRemover.innerText = "Remover"
+    botaoRemover.classList.add("botaoRemover");
+
+    const celulaStatusTarefa = document.createElement("td");
+    celulaStatusTarefa.id = "celulaStatusTarefa"
+    celulaStatusTarefa.appendChild(botaoStatus);
+    celulaStatusTarefa.appendChild(botaoRemover);
+
+    linha.appendChild(celulaNomeTarefa);
+    linha.appendChild(celulaDataTarefa);
+    linha.appendChild(celulaStatusTarefa);
+
+    //adicionando linha
+    const corpoTabela = document.querySelector("#corpoTabela");
+    corpoTabela.appendChild(linha);
+
+    //removendo linha
+    botaoRemover.addEventListener("click", function(removeLinha){
+        // console.log("entrei");
+        corpoTabela.removeChild(linha);
+    })
+
+    //concluindo tarefa
+    botaoStatus.addEventListener("click", function(concluiTarefa){
+        tarefa.status = "Concluido"
+        botaoStatus.innerText = tarefa.status;
+    })
+
+    
 }
 
 
